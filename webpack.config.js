a9ccf7603639fac753449697a84e2fcc1ab8ea20
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2024-07-02 23:45:58 -0500

    start of the project
    need to work on the pages for the tabs

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                // loading the css
+                test: /\.css$/i,
+                use: ['style-loader', 'css.loader'],
+            },
+            {
+                // loading images
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                // loading fonts
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
