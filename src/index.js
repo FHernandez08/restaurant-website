f07d61e7e03ce6ee5b7b738dbd039313cc459265
Fidel Hernandez <fidelhernandez_8@hotmail.com>
2024-07-04 22:44:02 -0500

    created interactivity for buttons
    need to finish logic for menu and locations

--- src/index.js
+++ src/index.js
@@ -6,13 +6,30 @@ import { about } from "./about";
 import { menu } from "./menu";
 import { locations } from "./locations";
 
-const content = document.querySelector("#content");
-const homeBtn = document.querySelector(".home");
-const aboutBtn = document.querySelector(".about");
-const menuBtn = document.querySelector(".menu");
-const locationsBtn = document.querySelector(".locations");
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('#content');
+    const homeBtn = document.querySelector('.home');
+    const aboutBtn = document.querySelector('.about');
+    const menuBtn = document.querySelector('.menu');
+    const locationsBtn = document.querySelector('.locations');
 
-homeBtn.addEventListener('click', {
+    homeBtn.addEventListener('click', () => {
+        content.innerHTML = ``;
+        content.appendChild(homePage());
+    });
+
+    aboutBtn.addEventListener('click', () => {
+        content.innerHTML = ``;
+        content.appendChild(about());
+    });
     
-})
+    menuBtn.addEventListener('click', () => {
+        content.innerHTML = ``;
+        content.appendChild(menu);
+    })
 
+    locationsBtn.addEventListener('click', () => {
+        content.innerHTML = ``;
+        content.appendChild(locations);
+    })
+})
