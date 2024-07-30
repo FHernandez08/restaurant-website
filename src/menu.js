861a15e09f50adc5f39a3e6f8e249963b590d220
Fidel A Hernandez Jr <fidelhernandez_8@hotmail.com>
2024-07-29 23:32:00 -0500

    add images for menu

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,15 @@
 // page tab for the menu tab
 
 // images
+import americano from './assets/americano.jpg';
+import espresso from './assets/espresso.jpg';
+import cappuccino from './assets/cappuccino.jpg';
+import latte from './assets/latte.jpg';
 
+import empanadas from './assets/empanadas.jpg';
+import tamales from './assets/tamales.jpg';
+import bunuelos from './assets/bunuelos.jpg';
+import bandejaPaisa from './assets/bandeja-paisa.jpg';
 
 // main function
 export default function menu() {
